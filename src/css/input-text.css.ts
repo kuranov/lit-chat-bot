3674b8630ebf7f84093424646c934cecead0ba23
anton <anton@triber.app>
2022-01-02 00:27:01 +0200

    open wc scaffold and moved styles to exported consts

---
+++ src/css/input-text.css.ts
@@ -0,0 +1,11 @@
+import {css} from "lit";
+
+export const inputTextCss = css`
+input, input:focus {
+  border: 1px solid #fff;
+  border-radius: 8px 0 0 8px ;
+  padding: 12px;
+  font-size: 24px;
+  background: transparent;
+  outline: none;
+}`;
---
+++ src/models/user.model.ts
@@ -0,0 +1,4 @@
+interface UserModel {
+  name: string;
+  isBot: boolean;
+}
