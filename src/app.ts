ee0e31293a929eac1aa8fb38abd4e0ab52f8cae7
anton <anton@triber.app>
2022-01-03 19:32:41 +0200

    chat room appearing animation

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,37 @@
 import {html, LitElement} from 'lit';
-import {customElement, state} from 'lit/decorators.js';
+import {customElement, property, state} from 'lit/decorators.js';
 import "./components/login-form.js";
 import "./components/app-container.js";
 import "./components/chat-room.js";
 import {SignController} from "./controllers/sign.controller";
+import {animate, AnimateController, fadeOut, flyAbove, flyBelow} from "@lit-labs/motion";
+import {TemplateResult} from "lit-html";
 
 @customElement('app-index')
 export class AppElement extends LitElement {
+  controller = new AnimateController(this, {
+    defaultOptions: {
+      keyframeOptions: {
+        fill: 'backwards',
+      },
+    },
+  });
+
+  private animations = {
+    chatRoom: {
+      keyframeOptions: {
+        delay: 100,
+        duration: 400
+      },
+      in: flyAbove,
+      out: flyBelow
+    },
+    login: {
+      duration: 100,
+      out: flyAbove
+    }
+  };
+
   @state()
   currentMember!: MemberModel;
 
@@ -20,8 +45,14 @@ export class AppElement extends LitElement {
 
   override render() {
     const content = this.currentMember
-      ? html`<chat-room .currentMember=${this.currentMember}></chat-room>`
-      : html`<login-form @login=${this.onLogin}></login-form>`;
+      ? html`<chat-room 
+            .currentMember=${this.currentMember}
+            ${animate(this.animations.chatRoom)}
+        ></chat-room>`
+      : html`<login-form 
+            @login=${this.onLogin}
+            ${animate(this.animations.login)}
+        ></login-form>`;
 
     return html`<app-container>${content}</app-container>`;
   }
