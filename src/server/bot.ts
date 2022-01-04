9bc3a0b2b0b66d6de6f569397c9cf1e41d136189
anton <anton@triber.app>
2022-01-05 00:19:08 +0200

    bot reactions ready

--- src/server/bot.ts
+++ src/server/bot.ts
@@ -12,11 +12,15 @@ export class Bot {
 
   state: BotState = BotState.NEUTRAL;
 
+  pendingQuestion?: MessageModel;
+
   profile: MemberModel = {
     name: 'James Bot',
     isBot: true,
     avatar: avatarGenerator([], true)
   }
+
+  questionsAndAnswers: QuestionAndAnswerModel[] = [];
 
   constructor(private messagePublisher: PublishMessageMethod) {}
 
@@ -29,11 +33,15 @@ export class Bot {
   }
 
   onMemberRegister(member: MemberModel): void {
-
+    this.publish(this.createRandom([
+      `${member.name}, haven't seen you here before! Glad to see you!`,
+      `Wow, new faces! Hi, ${member.name}!`,
+      `Welcome abroad, ${member.name}!`,
+    ]));
+    return;
   }
 
   onMemberOnline(member: MemberModel): void {
-
   }
 
   onMessage(message: MessageModel): void {
@@ -41,14 +49,82 @@ export class Bot {
       return;
     }
 
-    if (this.isGreeting(message)) {
-      return this.sayHi(message);
+    if (this.state === BotState.PENDING_FOR_ANSWER) {
+      return this.handlePendingForAnswer(message);
+    }
+
+    if (this.state === BotState.PENDING_FOR_ANSWER_CONFIRMATION) {
+      return this.handlePendingForAnswerConfirmation(message);
     }
 
     if (this.isQuestion(message)) {
-      this.sayHi(message);
+      this.pendingQuestion = message;
+      this.state = BotState.PENDING_FOR_ANSWER;
       return;
     }
+
+    if (this.isGreeting(message)) {
+      return this.sayHi(message);
+    }
+  }
+
+  handlePendingForAnswer(message: MessageModel): void {
+    [
+      'Seems like it is answer for previous question, right?',
+      `Question: "${this.pendingQuestion?.text}"`,
+      `Save answer? Type 'Yes' or 'No'`,
+    ]
+    .forEach(txt => this.publish(this.createMessage(txt)));
+
+    this.state = BotState.PENDING_FOR_ANSWER_CONFIRMATION;
+  }
+
+  handlePendingForAnswerConfirmation(message: MessageModel): void {
+    if (!this.pendingQuestion) {
+      return;
+    }
+
+    const [yes, no] = [this.isYes(message), this.isNo(message)];
+
+    if (!yes && !no) {
+      this.publish(this.createRandom([
+        'Please, just Yes or No',
+        'Not clear… I\'m just a robot, help me to understand you, say Yes or No',
+        'Hm-m, I wait for Yes or No, simple deal',
+      ]));
+      return;
+    }
+
+    if (yes) {
+      this.questionsAndAnswers.push({
+        question: this.pendingQuestion!,
+        answer: message,
+      });
+      this.publish(this.createRandom([
+        'Okay, got it!',
+        'Yep, saved!',
+        'I will keep it!',
+      ]));
+    }
+
+    if (no) {
+      this.publish(this.createRandom([
+        'Nevermind…',
+        ':(',
+        'Fine.'
+      ]));
+    }
+
+    this.pendingQuestion = undefined;
+    this.state = BotState.NEUTRAL;
+  }
+
+  isYes(message: MessageModel): boolean {
+    return /^yes/i.test(message.text.trim());
+  }
+
+  isNo(message: MessageModel): boolean {
+    return /^no/i.test(message.text.trim());
   }
 
   isGreeting(message: MessageModel): boolean {
@@ -63,8 +139,8 @@ export class Bot {
 
   sayHi(message: MessageModel): void {
     const response = this.createRandom([
-      `Hello there!`,
-      `Hey, ${message.username}! Whats up?`
+      `${message.username}, glad to see you here :)`,
+      `How it's going, ${message.username}?`,
     ]);
     this.publish(response);
   }
